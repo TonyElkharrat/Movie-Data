d4ced4e7657dbeaa2dbf3b9d902adb47ae2ce945
Tony Elkharrat <tonyel432@gmail.com>
2024-02-03 18:16:57 +0200

    make hhtps

--- src/SelectedMovie/SelectedMovie.js
+++ src/SelectedMovie/SelectedMovie.js
@@ -44,7 +44,7 @@ const SelectedMovie = ({
     async function getMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setIsLoading(false);
